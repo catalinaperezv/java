ef87e50c20b689399c53b273e773cd9accab7ca9
Catalina-perez <cata.2010.1000000@gmail.com>
2025-05-22 16:50:27 -0400

    Add files via upload

---
+++ PeliculasYDirectores1Application.java
@@ -0,0 +1,12 @@
+package com.catalina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasYDirectores1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(PeliculasYDirectores1Application.class, args);
+    }
+}
+
---
+++ ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.catalina;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PeliculasYDirectores1Application.class);
+	}
+
+}
